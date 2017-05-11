7e3bcaddc2d9ed5325162a4473e01660dfa5f70f
Seiki koga <alfr0475@gmail.com>
2017-05-11 18:01:15 +0900

    未読数の表示対応

--- webview.js
+++ webview.js
@@ -1,25 +1,13 @@
-// module.exports = (Franz, options) => {
-//     let first_stmt = $($('#statements .statement')[0]).attr('class');
-//     Franz.latestStatement = $('div#statements .statement')[0].className;
+module.exports = (Franz, options) => {
 
-//     $(window).bind('scroll', (ev) => {
-//         Franz.latestStatement = $('div#statements .statement')[0].className;
-//     });
+    function getMessages() {
+        var reply  = 0;
+        $('.list_item_talk .new_chat_count').each(function () {
+            reply += parseInt($(this).text());
+        });
 
-//     function getMessages() {
-//         const res = $('#new-res-notice-text').text().match(/新着レスが(\d+)?個あります/);
-//         var reply = 0;
-//         if (res) {
-//             reply = res[1];
-//         }
-//         var unread = 0;
-//         const ls = $('div#statements .statement')[0].className;
-//         if (ls != Franz.latestStatement) {
-//             unread = 1;
-//         }
+        Franz.setBadge(reply);
+     }
 
-//         Franz.setBadge(reply, unread);
-//     }
-
-//     Franz.loop(getMessages);
-// }
+     Franz.loop(getMessages);
+}
